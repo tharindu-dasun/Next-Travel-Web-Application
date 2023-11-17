5690d170ae62ebce10a726d3bc0965109a86d152
tharindu-dasun <tharindu.dasunccp@gmail.com>
2023-11-18 00:07:58 +0530

    doPost

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -8,10 +8,23 @@ import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = "/testing")
 public class Test extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("Done");
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        System.out.println("Done");
+//
+//        String name_input = req.getParameter("name_input");
+//        System.out.println(name_input);
+//
+//        PrintWriter writer = resp.getWriter();
+//        writer.println("<h1> Hello, " + name_input + "!</h1>");
+//
+//        writer.println(" <form method=\"get\" action=\"index.html\">\n" +
+//                "        <button type=\"submit\">Go Back</button>\n" +
+//                "    </form> ");
+//    }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name_input = req.getParameter("name_input");
         System.out.println(name_input);
 
