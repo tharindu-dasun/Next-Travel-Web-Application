83a992a7ca6c8d0911ae2c292a31d080286344e6
tharindu-dasun <tharindu.dasunccp@gmail.com>
2023-11-18 00:00:12 +0530

    doGet update 0.1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,11 +4,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = "/testing")
 public class Test extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("Done");
+
+        String name_input = req.getParameter("name_input");
+        System.out.println(name_input);
+
+        PrintWriter writer = resp.getWriter();
+        writer.println("<h1> Hello, " + name_input + "!</h1>");
+
+        writer.println(" <form method=\"get\" action=\"index.html\">\n" +
+                "        <button type=\"submit\">Go Back</button>\n" +
+                "    </form> ");
     }
 }
